98d27aaa6ec1e9a72c7a2df7489577172c1930ee
Samelena <lyly-miranda@yandex.ru>
2021-05-30 14:30:43 +0300

    Initial commit

---
+++ src/main/java/Yandexweather/YandexWeatherTestDrive.java
@@ -0,0 +1,45 @@
+package Yandexweather;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static okhttp3.RequestBody.*;
+
+public class YandexWeatherTestDrive {
+
+        public static void main(String[] args) throws IOException {
+            /*GET https://api.weather.yandex.ru/v2/forecast?
+  lat=<54.70739>
+ & lon=<20.507307>
+ & [lang=<"ru_RU">]
+ & [limit=<7>]
+ & [hours=<«true»>]
+ & [extra=<«true»>]
+
+X-Yandex-API-Key: <a653e609-ef9b-4cb8-b198-a04108bceafb>*/
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+
+            String requestBody = "GET https://weather.yandex.ru/v2/informers?lat=54.70739&lon=20.507307&extra=true" +
+                    " & [lang=<\"ru_RU\">]\n" +
+                    " & [limit=<7>]\n" +
+                    " & [hours=<«true»>]\n" +
+                    " & [extra=<«true»>]\n" +
+                    "\n" +
+                    "X-Yandex-API-Key: <a653e609-ef9b-4cb8-b198-a04108bceafb>";
+
+            MediaType JSON = MediaType.parse("JSON");
+            Request request = new Request.Builder()
+                    .url("https://api.weather.yandex.ru")
+                    .build();
+            Response response = okHttpClient.newCall(request).execute();
+            String body = response.body().string();
+
+        }
+}
